88f42133f356581f30aaee302cb442c9baa381be
franco.gonzalez <francogonzalezsanchez@alu.uma.es>
2020-05-29 01:13:50 +0200

    Creation of a constructor and a method

--- 12_2_ReadUpperAndLower/ReadUpperAndLower/Program.cs
+++ 12_2_ReadUpperAndLower/ReadUpperAndLower/Program.cs
@@ -13,11 +13,21 @@ namespace ReadUpperAndLower
         private string[] tempUp;
         private string[] tempLo;
 
+        public Program(string str)
+        {
+            this.str = str;
+            n = str.Length;
+            Uppers = new string[n];
+            Lowers = new string[n];
+            tempUp = new string[n];
+            tempLo = new string[n];
+        }
+
         private String uppers()
         {
             for (int i = 0; i < n; i++)
             {
-                if (Char.IsUpper(str[i]))
+                if (Char.IsUpper(str[i])) // If the character is Uppercased
                 {
                     tempUp[i] = Convert.ToString(str[i]);
                 }
@@ -33,9 +43,33 @@ namespace ReadUpperAndLower
             }
             return seeUppers;
         }
+        private String lowers()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (Char.IsLower(str[i])) // If the character is Lowercased
+                {
+                    tempLo[i] = Convert.ToString(str[i]);
+                }
+
+            }
+            for (int i = 0; i < tempLo.Length; i++)
+            {
+                if (tempLo[i] != null)
+                {
+                    Lowers[i] = tempLo[i];
+                    seeLowers += $",{Lowers[i]}";
+                }
+            }
+            return seeLowers;
+        }
         static void Main(string[] args)
         {
-            var prog = new Program();
+            Console.Write("Write a line:");
+            String str = Console.ReadLine();
+            var prog = new Program(str);
+
+
         }
     }
 }
