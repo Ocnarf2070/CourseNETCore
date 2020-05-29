9cac5b94bb67dc1dc9a46b9072cd973e44971cea
franco.gonzalez <francogonzalezsanchez@alu.uma.es>
2020-05-29 02:27:44 +0200

    Project about overloading methods

---
+++ 12_3_MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        public Program() //Empty Constructor
+        {
+
+        }
+        public Program(string name) //Overloaded Constructor
+        {
+
+        }
+        public Program(string name, int age) //Second Overloaded Constructor
+        {
+
+        }
+        public Program(int age, string name) //Inverted Second Overloaded Constructor 
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            var data = new Program(); // Execute the Empty Constructor
+            var data2 = new Program("Rio"); // Execute the Overloaded Constructor
+            var data3 = new Program("Jose", 23); // Execute the Second Overloaded Constructor
+            var data4 = new Program(23, "Jose"); // Execute the Inverted Second Overloaded Constructor
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
