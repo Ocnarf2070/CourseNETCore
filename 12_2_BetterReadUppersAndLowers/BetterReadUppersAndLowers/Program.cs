cf66a88757284e8a0826488cf1e6b25fcd3dba9a
franco.gonzalez <francogonzalezsanchez@alu.uma.es>
2020-05-29 02:16:28 +0200

    Uploading a better implementation of previous project

---
+++ 12_2_BetterReadUppersAndLowers/BetterReadUppersAndLowers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BetterReadUpperAndLower
+{
+    class Program
+    {
+        //Atributes
+        private String str;
+
+        //Constructor
+        public Program(string str)
+        {
+            this.str = str;
+        }
+
+        private String uppers()
+        {
+            string Uppers = "";
+            foreach(var c in str)
+            {
+                if (Char.IsUpper(c))
+                {
+                    Uppers += $",{c}";
+                }
+            }
+            if (Uppers.Length > 0) Uppers = Uppers.Substring(1);
+            return Uppers;
+        }
+        private String lowers()
+        {
+            string Lowers = "";
+            foreach (var c in str)
+            {
+                if (Char.IsLower(c))
+                {
+                    Lowers += $",{c}";
+                }
+            }
+            if (Lowers.Length > 0) Lowers = Lowers.Substring(1);
+            return Lowers;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Write a line:");
+            String str = Console.ReadLine();
+            var prog = new Program(str);
+            Console.WriteLine("Uppercased characters are: " + prog.uppers());
+            Console.WriteLine("Lowercased characters are: " + prog.lowers());
+
+        }
+    }
+}
