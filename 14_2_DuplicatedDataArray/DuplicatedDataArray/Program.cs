6de1b5d72727fa4a65da86738e6e1edccdc31d7e
franco.gonzalez <francogonzalezsanchez@alu.uma.es>
2020-05-31 02:40:20 +0200

    Upload project with a example of utlization of a function

---
+++ 14_2_DuplicatedDataArray/DuplicatedDataArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DuplicatedDataArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 2, 3, 6, 4, 4, 4, 9, 5, 6, 5, 9, 2 };
+            int[] aux = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        count++;
+                        if (number(array[i])) aux[i] = array[i];
+                    }
+                }
+                if (aux[i] != 0) Console.WriteLine($"{aux[i]} se repite: {count}");
+            }
+            bool number(int num)
+            {
+                for (int i = 0; i < aux.Length; i++)
+                {
+                    if (aux[i] == num) return false;
+                }
+                return true;
+            }
+            
+        }
+    }
+}
