726f84a5dbaa5d2c31774a37491dca337d651e4d
franco.gonzalez <francogonzalezsanchez@alu.uma.es>
2020-05-31 17:47:09 +0200

    Implementation of the Main program

--- 15_2_VelocityConversor/VelocityConversor/Program.cs
+++ 15_2_VelocityConversor/VelocityConversor/Program.cs
@@ -6,7 +6,37 @@ namespace VelocityConversor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int option;
+            char repeat = 'y';
+            var conv = new Conversor();
+            do
+            {
+                Console.WriteLine("Choose and option:\n" +
+                "\t1 - Convert m/s to km/h\n" +
+                "\t2 - Convert km/h to m/s");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        conv.msToKh();
+                        break;
+                    case 2:
+                        conv.khToMs();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+                Console.WriteLine("Do you want to exit the program? Y/n");
+                repeat = Convert.ToChar(Console.ReadLine().ToLower());
+                while (repeat != 'y' && repeat != 'n') //(!(repeat == y || repeat == n))
+                {
+                    Console.WriteLine("Invalid input.\nPlease introduce y to continue or n to exit.");
+                    repeat = Convert.ToChar(Console.ReadLine().ToLower());
+                }
+            } while (repeat=='y');
+            
+            
         }
     }
 
