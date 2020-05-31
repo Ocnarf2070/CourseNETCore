4bf776261ad4e064cfed20f7ec17051cf436d659
franco.gonzalez <francogonzalezsanchez@alu.uma.es>
2020-05-31 17:25:39 +0200

    Uploading first part of the project which utilize the knoledge about static

---
+++ 15_2_VelocityConversor/VelocityConversor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VelocityConversor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Conversor
+    {
+        private double velocity;
+
+        public void msToKh()
+        {
+            Console.Write("Introduce a velocity in m/s: ");
+            velocity = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{velocity} m/s = {velocity * 3600 / 1000} km/h");
+        }
+        public void khToMs()
+        {
+            Console.Write("Introduce a velocity in km/h: ");
+            velocity = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{velocity} km/h = {velocity * 1000 / 3600} m/s");
+
+        }
+    }
+}
