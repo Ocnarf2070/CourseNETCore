5823fbf80da96fe51a8de4f37f6922fcaa19c22b
franco.gonzalez <francogonzalezsanchez@alu.uma.es>
2020-06-02 18:27:46 +0200

    Updating a file

--- 17_2_School/School/School.cs
+++ 17_2_School/School/School.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using CSharpPropieties;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace School
 {
-    class School
+    public class School
     {
+        private List<Studients> studients;
+        public School()
+        {
+            studients = new List<Studients>();
+        }
+
+        public void addStudient(Studients newStudient)
+        {
+            studients.Add(newStudient);
+        }
+        public bool searchStudient(String name)
+        {
+            int i = 0;
+            while (i < studients.Count && !studients[i].Name.Equals(name)) i++;
+            return i < studients.Count;
+        }
+            
+        
     }
 }
