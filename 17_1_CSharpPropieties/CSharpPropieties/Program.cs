d576bc7568e8f6aa4392feaa1978fffdf8c9ade5
franco.gonzalez <francogonzalezsanchez@alu.uma.es>
2020-06-02 17:53:11 +0200

    Uploading project about propieties of the object (Classes)

---
+++ 17_1_CSharpPropieties/CSharpPropieties/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpPropieties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Propieties Part 1
+            var studient = new Studients();
+            //var name = studient.name; Creation simple of the object
+            studient.name = "Jose"; // Object now has a proper set
+
+            //Propieties Part 2
+            Console.WriteLine("Name of the studient: "+studient.name);//Object now has a proper get
+
+        }
+    }
+}
---
+++ 17_1_CSharpPropieties/CSharpPropieties/Studients.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpPropieties
+{
+    public class Studients
+    {
+        //Part 1
+        //public String name { get; set; } = "Jesus";
+        private String nameaux;
+        public String name
+        {
+            //set { nameaux = value; }
+            //Part 2
+            //get { return nameaux; }
+
+            get => nameaux;
+            set => nameaux = value;
+        }
+    }
+}
