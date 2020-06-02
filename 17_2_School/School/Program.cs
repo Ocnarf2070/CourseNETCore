a6e52ac0b82d5e412761852ce89e2777599a05d6
franco.gonzalez <francogonzalezsanchez@alu.uma.es>
2020-06-02 18:23:38 +0200

    Upload project which utilize the propieties of an object

---
+++ 17_2_School/School/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpPropieties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var studient = new Studients();
+            studient.Name = "Jose"; 
+
+            Console.WriteLine("Name of the studient: "+studient.Name);
+
+        }
+    }
+}
---
+++ 17_2_School/School/Studients.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpPropieties
+{
+    public class Studients
+    {
+        public int Age { get; set; }
+        public String Name { get; set; }
+        public double Score { get; set; }
+
+
+    }
+}
