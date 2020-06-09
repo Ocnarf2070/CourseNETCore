52b5baaad330d4a16149741cc0c7cf4ceb3bab29
franco.gonzalez <francogonzalezsanchez@alu.uma.es>
2020-06-09 02:27:18 +0200

    Uploading project about the implementation of a LinkedList with nodes

--- 19_LinkedListNode/LinkedListNode/Program.cs
+++ 19_LinkedListNode/LinkedListNode/Program.cs
@@ -6,7 +6,19 @@ namespace LinkedListNode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] items = { 2, 4, 1, 3, 5, 4 };
+            LinkedListN list = new LinkedListN();
+            EmptyList(list);
+            foreach (var item in items)
+            {
+                list.Add(item);
+                Console.WriteLine(list);
+            }
+        }
+        public static void EmptyList(LinkedListN list)
+        {
+            if (list.IsEmpty()) Console.WriteLine("The list is empty");
+            else Console.WriteLine("The list has elements");
         }
     }
 }
