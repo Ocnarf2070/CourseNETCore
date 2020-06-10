bf37d655ff39397e8da15ca4e8d122db9eee07e5
franco.gonzalez <francogonzalezsanchez@alu.uma.es>
2020-06-11 00:09:16 +0200

    Uploading project about Inheritance

---
+++ 20_Inheritance/Inheritance/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Menu
+    {   
+        //Part 1
+        public void getProduct()
+        {
+            Console.WriteLine("getProduct");
+        }
+
+        //Part 2
+        private string s;
+
+        public Menu(string s)
+        {
+            this.s = s;
+        }
+
+        protected virtual void GetProduct()
+        {
+            Console.WriteLine(s);
+        }
+
+
+    }
+}
---
+++ 20_Inheritance/Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program : Menu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Class-based Inheritance");
+            new Program("").getProduct(); //If the father class method is not public, it cannot be called by their childs
+
+            Console.WriteLine("Keywords: protected, base, override and virtual");
+            new Program("Empresa: ADA").GetProduct(); //Although is not a public method, the child can access because it protected. Only child from the father class can access this method
+        }
+
+        //Part 2
+        public Program(String s) : base(s)
+        {
+
+        }
+        protected override void GetProduct()
+        {
+            Console.WriteLine("GetProductOverrided");
+        }
+    }
+}
